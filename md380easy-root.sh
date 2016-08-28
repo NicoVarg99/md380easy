ab74dcd4f2444c56378b9b72e72ca88c21771552
NicoVarg99 <nicolasalsotto@gmail.com>
2016-08-29 01:24:26 +0200

    Create md380easy-root.sh

---
+++ md380easy-root.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+  if [ -f /usr/bin/md380 ]; then
+    bash /usr/bin/md380
+  else
+	  echo "[ERROR] This script must be run as root." >&2 
+	  exit 1
+	fi
+fi
