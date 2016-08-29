a83b6c907fc4db3fbccb60aa9be445255bf99dae
NicoVarg99 <nicolasalsotto@gmail.com>
2016-08-29 15:46:45 +0200

    Update md380easy-user.sh

--- md380easy-user.sh
+++ md380easy-user.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
+export RED='\033[1;91m'
+export GREEN='\033[1;92m'
+export BLUE='\033[1;94m'
+export RESETCOLOR='\033[1;00m'
+export TXTINFO="$GREEN[INFO]$RESETCOLOR"
+export TXTERROR="$RED[ERROR]$RESETCOLOR"
+export TXTQST="$BLUE[QUESTION]$RESETCOLOR"
+
 if [ $(id -u) -eq 0 ]; then
     sudo bash /usr/sbin/md380 $1
     exit 0
 fi
-
-
 
 
 
