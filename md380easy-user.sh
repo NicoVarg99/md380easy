579ffdbaef643c6458bd831d4d2e403f80ab1025
NicoVarg99 <nicolasalsotto@gmail.com>
2016-08-29 15:44:51 +0200

    Update md380easy-user.sh

--- md380easy-user.sh
+++ md380easy-user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(id -u) -eq 0 ]; then
-    sudo bash /usr/sbin/md380
+    sudo bash /usr/sbin/md380 $1
     exit 0
 fi
 
@@ -10,3 +10,11 @@ fi
 
 
 echo md380easy-user
+
+if [[ "$1" == "--uninstall" ]]
+then
+    if [ $(id -u) -ne 0 ]; then
+	    echo -e "$TXTERROR This script must be run as root." >&2
+	    exit 1
+    fi
+fi
