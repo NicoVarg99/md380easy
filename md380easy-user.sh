096af0ddab7b9ae7617749552694de9f0ce1ab17
NicoVarg99 <nicolasalsotto@gmail.com>
2016-08-29 15:47:14 +0200

    Update md380easy-user.sh

--- md380easy-user.sh
+++ md380easy-user.sh
@@ -21,7 +21,7 @@ echo md380easy-user
 if [[ "$1" == "--uninstall" ]]
 then
     if [ $(id -u) -ne 0 ]; then
-	    echo -e "$TXTERROR This script must be run as root." >&2
-	    exit 1
+      echo -e "$TXTERROR This script must be run as root." >&2
+      exit 1
     fi
 fi
