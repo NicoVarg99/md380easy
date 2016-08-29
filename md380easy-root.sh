7d9659be465e2d94203257c17a5dc17978c52c5f
NicoVarg99 <nicolasalsotto@gmail.com>
2016-08-29 15:42:49 +0200

    Update md380easy-root.sh

--- md380easy-root.sh
+++ md380easy-root.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+export RED='\033[1;91m'
+export GREEN='\033[1;92m'
+export BLUE='\033[1;94m'
+export RESETCOLOR='\033[1;00m'
+export TXTINFO="$GREEN[INFO]$RESETCOLOR"
+export TXTERROR="$RED[ERROR]$RESETCOLOR"
+export TXTQST="$BLUE[QUESTION]$RESETCOLOR"
+
+function fnuninstall {
+	rm -rf ~/md380tools
+	rm /etc/udev/rules.d/99-md380.rules
+	echo -e "$TXTINFO md380 uninstalled"
+	rm /usr/bin/md380
+	rm /usr/sbin/md380
+}
+
+
 if [ $(id -u) -ne 0 ]; then
   if [ -f /usr/bin/md380 ]; then
     bash /usr/bin/md380
@@ -11,3 +28,9 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 echo md380easy-root
+
+if [[ "$1" == "--uninstall" ]]
+then
+        echo -e "$TXTINFO will now uninstall md380easy"
+	fnuninstall
+fi
